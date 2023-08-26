9a05546634cdbf60ba242394330cee6a39bf4bb5
speedcoder107 <85942485+speedcoder107@users.noreply.github.com>
2023-08-26 02:17:25 +0000

    update header for phone

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('.grtweet').addEventListener('mouseover', function(event){
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.grtweet').addEventListener('mouseover', function (event) {
         document.querySelector('#whiteTwitter').style.display = 'none';
         document.querySelector('#grayTwitter').style.display = 'inline'
         event.preventDefault();
     })
-    document.querySelector('.grtweet').addEventListener('mouseout', function(event){
+    // if (window.innerWidth <= 984) {
+
+        document.querySelector('.headerbutton').addEventListener('click', function (event) {
+            if (document.querySelector('.headerAbout').style.display == 'none') {
+                document.querySelector('.headerAbout').style.display = 'block';
+                document.querySelector('.headerLife').style.display = 'block';
+                document.querySelector('.headerMemories').style.display = 'block';
+                document.querySelector('.headerResume').style.display = 'block';
+                document.querySelector('.headerConnect').style.display = 'block';
+            }
+            else if(document.querySelector('.headerAbout').style.display == 'block'){
+                document.querySelector('.headerAbout').style.display = 'none';
+                document.querySelector('.headerLife').style.display = 'none';
+                document.querySelector('.headerMemories').style.display = 'none';
+                document.querySelector('.headerResume').style.display = 'none';
+                document.querySelector('.headerConnect').style.display = 'none';
+            }
+            event.preventDefault();
+        })
+    // }
+    document.querySelector('.grtweet').addEventListener('mouseout', function (event) {
         document.querySelector('#whiteTwitter').style.display = 'inline';
         document.querySelector('#grayTwitter').style.display = 'none'
         event.preventDefault();
     })
-    document.querySelector('.grface').addEventListener('mouseover', function(event){
+    document.querySelector('.grface').addEventListener('mouseover', function (event) {
         document.querySelector('#whiteFacebook').style.display = 'none';
         document.querySelector('#grayFacebook').style.display = 'inline'
         event.preventDefault();
     })
-    document.querySelector('.grface').addEventListener('mouseout', function(event){
+    document.querySelector('.grface').addEventListener('mouseout', function (event) {
         document.querySelector('#whiteFacebook').style.display = 'inline';
         document.querySelector('#grayFacebook').style.display = 'none'
         event.preventDefault();
     })
-    document.querySelector('.grinsta').addEventListener('mouseover', function(event){
+    document.querySelector('.grinsta').addEventListener('mouseover', function (event) {
         document.querySelector('#whiteInsta').style.display = 'none';
         document.querySelector('#grayInsta').style.display = 'inline'
         event.preventDefault();
     })
-    document.querySelector('.grinsta').addEventListener('mouseout', function(event){
+    document.querySelector('.grinsta').addEventListener('mouseout', function (event) {
         document.querySelector('#whiteInsta').style.display = 'inline';
         document.querySelector('#grayInsta').style.display = 'none'
         event.preventDefault();
     })
-    })
+})
