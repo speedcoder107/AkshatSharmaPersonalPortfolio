e5d7e8f927230cf33ec9a0a31c4a3fc386217991
Akshat Sharma <aksharmaldh1947@gmail.com>
2026-03-17 16:45:47 -0400

    another change

--- src/data/data.ts
+++ src/data/data.ts
@@ -6,20 +6,19 @@ export const personalInfo = {
   linkedin: "https://www.linkedin.com/in/akshat-sharma-physics/",
   github: "https://github.com/speedcoder107",
   location: "College Park, Maryland",
-  shortBio: "I am a PhD student at the University of Maryland, focusing on Quantum Information Science. My passion lies in solving challenging problems—whether through physics, code, or logic puzzles.",
+  shortBio: "I am a PhD student at the University of Maryland, focusing on Quantum Information Science. My passion lies in solving challenging problems whether through physics, code, or logic puzzles.",
+
   about: [
     "Hi, my name is Akshat. I am currently a PhD student in physics at the University of Maryland, College Park. My research focuses on Quantum Information Science.",
     "During my time at the University of Minnesota, I discovered that I thrive from teaching. Whether it was tutoring at the SMART Learning Commons or lead mentoring through 'Rooted in STEM,' I've always been captivated by the 'aha!' moment when a complex concept suddenly clicks for a student.",
     "Now at UMD, I am leaning into Quantum Dimer Models and emergent phases of matter under Prof. Victor Galitski. I am fascinated by these new ideas realted to quantum materials and the potential they hold for future technologies. And I am excited to continue sharing my love of physics with others"
   ],
-  interests: [],
-  // interests: ["Logic Puzzles", "Cardistry", "Scientific Visualization", "Mentorship"],
+  interests: ["Logic Puzzles", "Cardistry", "Teaching"],
   highlights: [
     { title: "Mentorship", detail: "Rooted in STEM Lead Mentor" },
     { title: "Puzzle solving", detail: "Solving Rubick's Cubes" },
     { title: "Reading", detail: "Current Favorite Book: Recusion by Blake Crouch" }
   ],
-  // highlight: {title: string; detail: string }[] = []
 };
 
 export const education = [
